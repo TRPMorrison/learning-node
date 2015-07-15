629e86357e929d0c11a75c367b7b8a3ccb026752
Trini Morrison <TRPMorrison@gmail.com>
2015-07-15 10:17:57 -0400

    Added hits and count

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  NeDB = require('nedb'),
+  db = new NeDB('hits.db');
 
 app.set('view engine', 'jade');
 
 app.use(express.static(process.cwd() + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('index', {
-    myCoolMessage: 'Jade ROCKS!!!!'
+  addHit(function() {
+
+    countHits(function(theNumber) {
+      response.render('index', {
+        myCoolMessage: 'Jade ROCKS!!!!',
+        pageHits: theNumber
+      });
+    });
   });
 });
 
@@ -20,12 +28,42 @@ app.get('/greet/:name', function(req, res) {
     res.render('greet', {
       name: req.params.name
     });
-  }
-
-  else {
+  } else {
     res.status(404).render('404');
   }
 });
 
-console.log('listening on localhost:8025');
-app.listen(8025);
+db.loadDatabase(function(err) {
+  if (err) {
+    console.log('Error loading database:', err);
+  } else {
+    // console.log('listening on localhost:8025');
+    console.log('listening on localhost:8026');
+    app.listen(8026);
+  }
+});
+
+
+
+function addHit(cb) {
+  db.insert({
+    created_at: new Date().toString()
+  }, function(err) {
+    if (err) {
+      console.log('err adding hit', err);
+    } else {
+      cb();
+    }
+  });
+}
+
+
+function countHits(cb) {
+  db.count({}, function(err, count) {
+    if (err) {
+      console.log('err counting hits', err);
+    } else {
+      cb(count);
+    }
+  });
+}
